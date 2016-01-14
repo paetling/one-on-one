bd351109e5bbe36dccbf193f9f464b50eb6d2700
paetling <paetling@gmail.com>
2016-01-14 01:06:24 -0500

    allow users to set the date for the meeting

--- one_on_one/web_app/main.py
+++ one_on_one/web_app/main.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, render_template, request
+import arrow
 
 from one_on_one.group import GCGroup
 from one_on_one.pair import GCPair
@@ -32,7 +33,10 @@ def pairs():
 @app.route('/schedule', methods=["POST"])
 def schedule():
     pairs = json.loads(request.form["pairs"])
-    schedule_instance.schedule(pairs)
+    meeting_date_string = request.form["meeting_date"]
+    meeting_dt = arrow.get(meeting_date_string).datetime if meeting_date_string else None
+
+    schedule_instance.schedule(pairs, meeting_dt=meeting_dt)
     return render_template("schedule.html")
 
 if __name__ == "__main__":
